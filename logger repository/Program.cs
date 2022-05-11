ea21aa1485ee885ef984c5535a293b7cd88b583e
Angelina Butenko <angelinaa2712@gmail.com>
2022-05-11 22:53:20 +0300

    Add project files.

---
+++ logger repository/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace Logger
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        //    var starter = new Starter();
+        //    starter.Run();
+        }
+    }
+}
