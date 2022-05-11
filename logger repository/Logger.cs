c101b45cc21735f53aeb72bfd49cfd37f8356516
Angelina Butenko <angelinaa2712@gmail.com>
2022-05-11 22:55:35 +0300

    create logger

---
+++ logger repository/Logger.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Logger
+{
+    public class Logger
+    {
+        private static readonly Logger _instance = new Logger();
+        private readonly StringBuilder _logs;
+
+        static Logger()
+        {
+
+        }
+
+        private Logger()
+        {
+            _logs = new StringBuilder();
+        }
+
+        public static Logger Instance => _instance;
+
+        public string AllLogs => _logs.ToString();
+
+        public void LogInfo(string message)
+        {
+            Log(LogType.Info, message);
+        }
+
+        public void LogError(string message)
+        {
+            Log(LogType.Error, message);
+        }
+
+        public void LogWarning(string message)
+        {
+            Log(LogType.Warning, message);
+        }
+
+        public void Log(LogType type, string message)
+        {
+            var log = $"{DateTime.UtcNow} : {type.ToString()}: {message}";
+            _logs.AppendLine(log);
+            Console.WriteLine(log);
+        }
+    }
+}
+
---
+++ logger repository/Starter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    public class Starter
+    {
+        private const string FilePath = "log.txt";
+
+        private readonly Actions _action;
+        private readonly Logger _logger;
+
+        public Starter()
+        {
+            _action = new Actions();
+            _logger = Logger.Instance;
+        }
+
+        public void Run()
+        {
+            var random = new Random();
+            var result = new Result();
+
+            for (var i = 0; i < 100; i++)
+            {
+                switch (random.Next(maxValue: 3))
+                {
+                    case 0:
+                        result = _action.MethodOne();
+                        break;
+                    case 1:
+                        result = _action.MethodTwo();
+                        break;
+                    case 3:
+                        result = _action.MethodThree();
+                        break;
+                }
+
+                if (!result.Status)
+                {
+                    _logger.LogError($"Action failed by a reason: {result.ErrorMessage}");
+                }
+            }
+
+            File.WriteAllText(FilePath, _logger.AllLogs);
+        }
+    }
+}
